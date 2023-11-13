bbca2e8e89cf5c108eff2500b069f9c60b320f47
push42 <thiesmk2@gmail.com>
2023-11-13 20:53:22 +0100

    main upload

---
+++ dist/index.php
@@ -0,0 +1,44 @@
+<?php
+require '../vendor/autoload.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>FiveM - My Script Collection</title>
+        <meta name="description" content="Explore a collection of my scripts for FiveM. Purchase premium scripts for a bit less.">
+        <meta name="keywords" content="FiveM, scripts, gaming, premium scripts">
+        <meta name="author" content="Thies Bergenthal">
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/main.css">
+        <!-- Favicon -->
+        <link rel="icon" sizes="16x16" type="image/png" href="./assets/favicon-16x16.png">
+        <link rel="icon" sizes="32x32" type="image/png" href="./assets/favicon-32x32.png">
+        <link rel="icon" sizes="96x96" type="image/png" href="./assets/favicon-96x96.png">
+        
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta property="og:title" content="My Script Collection">
+        <meta property="og:description" content="Explore a collection of my scripts for FiveM. Purchase premium scripts for a bit less.">
+        <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/5/5a/FiveM-Logo.png">
+        <meta property="og:url" content="https://www.thiesbergenthal.com">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="My Script Collection">
+        <meta name="twitter:description" content="Explore a collection of my scripts for FiveM. Purchase premium scripts for a bit less.">
+        <meta name="twitter:image" content="https://upload.wikimedia.org/wikipedia/commons/5/5a/FiveM-Logo.png">
+        
+        <link rel="canonical" href="https://www.thiesbergenthal.de">
+
+    </head>
+        
+<body class="bg-zinc-950 font-sans">
+
+        
+        <script>
+
+        </script>
+    </body>
+</html>
